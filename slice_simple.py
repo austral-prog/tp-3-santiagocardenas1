b43c4dedbbc85a97488bdbf39d295d9ed25e6705
santiagocardenas1 <scardenas@mail.austral.edu.ar>
2025-04-06 17:58:47 -0300

    Update slice_simple.py

--- slice_simple.py
+++ slice_simple.py
@@ -3,4 +3,7 @@ def slice_simple():
     texto = texto.lower()
     print(texto[0:3])
     print(texto[2:5])
-    print(texto)
+    parte_1 = (texto[0:4])
+    parte_2 = (texto[-3:])
+    resultado = parte_1 + parte_2
+    print(resultado)
